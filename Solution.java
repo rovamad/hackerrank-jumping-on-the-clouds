0a8602558d3f07529ce63486dd90a201aba97864
rovamad <rojasmarcos@outlook.com>
2022-04-25 22:25:20 -0400

    Final commit of the excercise Done

---
+++ Solution.java
@@ -0,0 +1,62 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+class Result {
+
+    /*
+     * Complete the 'jumpingOnClouds' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER_ARRAY c as parameter.
+     */
+
+    public static int jumpingOnClouds(List<Integer> c) {
+        // Write your code here
+        List<Integer> arrayResult = new ArrayList<Integer>();
+
+        for (int i = 0; i < c.size(); i++) {
+            if (c.get(i)==0) {
+                arrayResult.add(c.get(i));
+                int possibleJump2 = i + 2;
+
+                if (possibleJump2 < c.size() && c.get(possibleJump2) != 1) {
+                    i = possibleJump2 - 1;
+                }
+            }
+        }
+        System.out.println(arrayResult);
+        return arrayResult.size() - 1;
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        String[] cTemp = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        List<Integer> c = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            int cItem = Integer.parseInt(cTemp[i]);
+            c.add(cItem);
+        }
+
+        int result = Result.jumpingOnClouds(c);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
